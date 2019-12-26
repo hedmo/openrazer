40d17fc7efc9797a7608c069a0e7cf4dc5892437
hedmo <hedmoo@yahoo.com>
2019-12-26 18:48:37 +0100

    Update mouse_mat.py

--- daemon/openrazer_daemon/hardware/mouse_mat.py
+++ daemon/openrazer_daemon/hardware/mouse_mat.py
@@ -9,7 +9,7 @@ class RazerFirefly(__RazerDeviceBrightnessSuspend):
     Class for the Razer Firefly (2013)
     """
     USB_VID = 0x1532
-    USB_PID = 0x0c00
+    USB_PID = 0x0C00
     HAS_MATRIX = True
     MATRIX_DIMS = [1, 15]
     METHODS = ['get_device_type_mousemat', 'set_wave_effect', 'set_static_effect', 'set_spectrum_effect',
